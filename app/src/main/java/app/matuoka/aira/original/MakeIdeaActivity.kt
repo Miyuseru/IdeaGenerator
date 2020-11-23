b874c52fe1d7a96efc8879bc25f649a162d8275d
miyuu kuwana <miyuu13k@gmail.com>
2020-11-23 21:52:40 +0900

    アイデア出し画面の作成

---
+++ app/src/main/java/app/matuoka/aira/original/MakeIdeaActivity.kt
@@ -0,0 +1,12 @@
+package app.matuoka.aira.original
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MakeIdeaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_make_idea)
+    }
+
+}
